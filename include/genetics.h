b487297f4795a915cad202a716e498e6251ec6ac
Esteban Campos <est-campos@estudiantec.cr>
2025-04-24 10:41:43 -0600

    First Commit.

---
+++ include/genetics.h
@@ -0,0 +1,31 @@
+#ifndef GENETICS_H
+#define GENETICS_H
+
+// #include <vector>
+#include <string>
+#include <cstring>
+// #include <cstdint>
+
+
+
+
+
+class Genetics
+{
+    public:
+        Genetics();
+
+        // void Show();
+
+        
+};
+
+
+
+
+
+
+
+
+
+#endif
---
+++ src/genetics.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+// #include <vector>
+#include <cstring>
+// #include <cstdint>
+// #include <iomanip>
+
