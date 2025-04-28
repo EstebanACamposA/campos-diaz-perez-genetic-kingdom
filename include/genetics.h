5b84308c8d60dbd824f7f39879687c47da477ef2
Esteban Campos <est-campos@estudiantec.cr>
2025-04-27 19:03:50 -0600

    Added Genetics class and testing function.

--- include/genetics.h
+++ include/genetics.h
@@ -8,8 +8,25 @@
 
 
 
+class Individual
+{
+    public:
+        Individual();
+        
+        // // stats:
+        // float mutation_relative_change = 0.2f;
+        // float health = 100;
+        // float speed = 10;
+        // float pierce_armor = 0;
+        // float magic_armor = 0;
+        // float siege_armor = 0;
 
 
+        
+        // void Show();
+
+
+};
 class Genetics
 {
     public:
@@ -17,7 +34,7 @@ class Genetics
 
         // void Show();
 
-        
+
 };
 
 
